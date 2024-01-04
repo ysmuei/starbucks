e0dd82ea5a61be98032db25d3783bb5509db9f9d
ysmuei <ysmuei@naver.com>
2024-01-04 23:44:05 +0900

    1월4일 header, section add

--- js/main.js
+++ js/main.js
@@ -14,3 +14,25 @@ searchInputEl.addEventListener('blur', function () {
   searchEl.classList.remove('focused');
   searchInputEl.setAttribute('placeholder', '');
 });
+
+
+const badgeEl = document.querySelector('header .badges');
+
+window.addEventListener('scroll', _.throttle(function () {
+  console.log(window.scrollY);
+  if(window.scrollY > 500) {
+    //배지 숨기기
+    //gsap.to(요소, 지속시간, 옵션);
+    gsap.to(badgeEl, .6, {
+      opacity: 0,
+      display: 'none'
+    })
+  }else {
+    //배지 보이기
+    gsap.to(badgeEl, .6, {
+      opacity: 1,
+      display: 'block'
+    })
+  }
+}, 300));
+// .throttle(함수, 시간) 부하를줌
