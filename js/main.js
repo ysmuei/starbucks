86fcb3af054276fb85d31d7333089e7de61c4761
ysmuei <ysmuei@naver.com>
2024-01-07 22:56:25 +0900

    1월7일 promotion toggle add

--- js/main.js
+++ js/main.js
@@ -73,3 +73,18 @@ new Swiper('.promotion .swiper', {
     nextEl: '.promotion .swiper-next'
   }
 });
+
+
+const promotionEl = document.querySelector('.promotion');
+const promotionToggleBtn = document.querySelector('.toggle-promotion');
+let isHidePromotion = false;
+promotionToggleBtn.addEventListener('click', function () {
+  isHidePromotion = !isHidePromotion // 값을 반대로 만들어줌
+  if (isHidePromotion) {
+    // 숨김 처리!
+    promotionEl.classList.add('hide')
+  } else {
+    // 보임 처리!
+    promotionEl.classList.remove('hide')
+  }
+});
